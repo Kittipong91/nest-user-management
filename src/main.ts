30c2805106771649efffccdda9e0777142857ed8
Kittipong <98655749+Kittipong91@users.noreply.github.com>
2025-12-18 06:47:51 +0700

    add ip

--- src/main.ts
+++ src/main.ts
@@ -28,6 +28,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
   
-  await app.listen(process.env.PORT ?? 8080);
+  await app.listen(process.env.PORT ?? 8080, '0.0.0.0');
 }
 bootstrap();
